ec6ecb77873a602b815008d54b1fbdc56f154b8f
Chek Ounkomol <chek.o@outlook.com>
2017-06-14 15:05:36 -0700

    Create separate random coordinate generator for selecting sub-arrays from larger arrays

--- gen_util.py
+++ gen_util.py
@@ -18,25 +18,40 @@ class CziLoader(object):
         if self.czi_reader.czi.axes == b'BCZYX0':
             return self.czi_np[0, c, :, :, :, 0]
 
-def extract_chunk(larger_ar, chunk_dims, coord):
+def pick_random_chunk_coord(shape, dims_chunk, dims_pin=(None, None, None)):
+    """Returns a random coordinate from where an array chunk can be extracted from a larger array.
+    
+    Parameters:
+    shape - tuple indicating shape of larger array
+    dims_chunk - tuple of chunk dimensions
+    dims_pin (optional) - tuple of fixed coordinate values. Dimensions for the returned coordinate
+      will be set to the dims_pin value if the value is not None.
+    
+    Returns:
+    coord - tuple of coordinate within larger array
+    """
+    assert len(shape) == len(dims_chunk)
+    coord = [0, 0, 0]
+    for i in range(len(dims_chunk)):
+        if dims_pin[i] is None:
+            coord[i] = np.random.random_integers(0, shape[i] - dims_chunk[i])
+        else:
+            coord[i] = dims_pin[i]
+    return tuple(coord)
+
+def extract_chunk(larger_ar, dims_chunk, coord):
     """Returns smaller array extracted from a larger array.
 
     Parameters:
     larger_ar - numpy.array
-    chunk_dims - tuple of chunk dimensions
+    dims_chunk - tuple of chunk dimensions
     coord - tuple to indicate coordinate in larger_ar to start the extraction. If None,
       a random valid coordinate will be selected
     """
-    if coord is None:
-        assert len(larger_ar.shape) == len(chunk_dims)
-        coord = [0, 0, 0]
-        for i in range(len(chunk_dims)):
-            coord[i] = np.random.random_integers(0, larger_ar.shape[i] - chunk_dims[i])
-    else:
-        assert len(larger_ar.shape) == len(chunk_dims) == len(coord)
+    assert len(larger_ar.shape) == len(dims_chunk) == len(coord)
     slices = []
     for i in range(len(coord)):
-        slices.append(slice(coord[i], coord[i] + chunk_dims[i]))
+        slices.append(slice(coord[i], coord[i] + dims_chunk[i]))
     return larger_ar[slices]
 
         
@@ -45,18 +60,36 @@ def main():
     print('main')
     czi_loader = CziLoader(fname)
     vol = czi_loader.get_volume(0)
-    print(type(vol))
-    print(vol.shape)
-    test = extract_chunk(vol, (2,3,4), (0, 0, 0))
-    print(test.shape)
-    print(test)
+    print('larger array shape:', vol.shape)
+    chunk = extract_chunk(vol, (2,3,4), (0, 0, 0))
+    print('chunk shape:', chunk.shape)
+    print(chunk)
 
-    # chunk same size as original
-    test = extract_chunk(vol, vol.shape, None)
-    print(test.shape)
+    # random coordinate selection
+    dims_chunk = (5,6,7)
 
-    for i in range(10):
-        test = extract_chunk(vol, (2,3,4), None)
+    n_runs = 10000
+    coords_random = np.zeros((n_runs, len(dims_chunk)), dtype=np.int)
+    print('generating', n_runs, 'random coordinates for chunk of shape', dims_chunk)
+    dims_pin = (None, None, None)
+    for i in range(n_runs):
+        coord = pick_random_chunk_coord(vol.shape, dims_chunk, dims_pin)
+        # print('random coordinate for chunk of size', dims_chunk, '->', coord)
+        coords_random[i] = coord
+        # test = extract_chunk(vol, dims_chunk, coord)
+        # print('extracted chunk shape:', test.shape)
+    print('random coord mins:', np.amin(coords_random, axis=0))
+    print('random coord maxs:', np.amax(coords_random, axis=0))
+    print('first 5 random coords:')
+    print(coords_random[:5])
+
+def test_czireader():
+    fname = './test_images/20161209_C01_001.czi'
+    reader = io.cziReader.CziReader(fname)
+    print('axes:', reader.czi.axes)
+    czi_np = reader.load()
+    print(type(czi_np), czi_np.shape)
 
 if __name__ == '__main__':
     main()
+    # test_czireader()
