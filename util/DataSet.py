5b0fe392bc61053a0e84a1a0d7c2140c3b32415c
Chek Ounkomol <chek.o@outlook.com>
2017-06-29 16:31:21 -0700

    Add DataSet split save/load

--- util/DataSet.py
+++ util/DataSet.py
@@ -1,25 +1,38 @@
 import os
+import pickle
 
 class DataSet(object):
-    def __init__(self, path, percent_test=0.1):
+    def __init__(self, path, percent_test=0.1, force_rebuild=False):
         """
         path - path to parent directory that contains folders of data.
         percent_test - (float between 0.0 and 1.0) percent of folders to be used as test set
         """
+        # member variables
         self._path = path
         self._percent_test = percent_test
+        self._all_set = None
+        self._test_set = None
+        self._train_set = None
         
+        self._save_path = os.path.join(os.path.dirname(path), 'dataset_saves', os.path.basename(path) + '.p')
+        if not os.path.exists(self._save_path) or force_rebuild:
+            self._build_new_sets()
+            self._save()
+        else:
+            self._load()
+
+    def _build_new_sets(self):
         self._all_set = [i.path for i in os.scandir(self._path) if i.is_dir()]  # order is arbitrary
         # TODO: shuffle self._all_set
-        idx_split = round(len(self._all_set)*percent_test)
-        if idx_split == 0:
-            idx_split = 1
-        self._test_set = self._all_set[:idx_split]
         if len(self._all_set) == 1:
             print('WARNING: DataSet has only one element')
-            idx_split = -1
-        self._train_set = self._all_set[idx_split:]
-
+            self._test_set = self._all_set
+            self._train_set = self._all_set
+        else:
+            idx_split = round(len(self._all_set)*self._percent_test)
+            self._test_set = self._all_set[:idx_split]
+            self._train_set = self._all_set[idx_split:]
+    
     def get_test_set(self):
         return self._test_set
     
@@ -30,6 +43,24 @@ class DataSet(object):
         # TODO: validate all folders
         pass
 
+    def _save(self):
+        dirname = os.path.dirname(self._save_path)
+        print(dirname)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+        package = (self._all_set, self._path, self._percent_test, self._test_set, self._train_set)
+        assert len(package) == 5
+        with open(self._save_path, 'wb') as fo:
+            pickle.dump(package, fo)
+            print('saved dataset split to:', self._save_path)
+
+    def _load(self):
+        with open(self._save_path, 'rb') as fin:
+            package = pickle.load(fin)
+        assert len(package) == 5
+        (self._all_set, self._path, self._percent_test, self._test_set, self._train_set) = package
+        print('loaded dataset split from:', self._save_path)
+
     def __str__(self):
         str_list = []
         str_list.append('DataSet from: ' + self._path)
@@ -39,24 +70,6 @@ class DataSet(object):
                                                                   len(self._all_set)))
         return os.linesep.join(str_list)
 
-def test():
-    print('testing DataSet')
-    data_path = '../data'
-    percent_test = 0.2
-    dataset = DataSet(data_path, percent_test=percent_test)
-    print(dataset)
-    test_set = dataset.get_test_set()
-    train_set = dataset.get_train_set()
-    test_tmp = set(test_set)
-    train_tmp = set(train_set)
-    union = test_tmp | train_tmp
-    intersect = test_tmp & train_tmp
-    print('union of test and train:', len(union))
-    print('intersection of test and train:', len(intersect))
-    assert len(union) == (len(test_set) + len(train_set))
-    assert len(intersect) == 0
-    print('*** TEST PASSED ***')
-
 if __name__ == '__main__':
-    test()
+    pass
     
