de934424021f84c63b8065895aee170abc870ab6
Chek Ounkomol <chek.o@outlook.com>
2017-07-06 10:35:37 -0700

    Move pytorch neural network definitions to separate files

--- model_modules/default_model.py
+++ model_modules/default_model.py
@@ -1,21 +1,25 @@
 import os
 import numpy as np
 import torch
-import torch.nn as nn
 import pickle
 import time
+import importlib
 
 CUDA = True
 
 class Model(object):
-    def __init__(self, mult_chan=None, depth=None, load_path=None, lr=0.0001):
+    def __init__(self, mult_chan=None, depth=None, load_path=None, lr=0.0001, nn_module='default_nn', init_weights=True):
+        nn_module = importlib.import_module('model_modules.nn_modules.' + nn_module)
+        
         self.criterion = torch.nn.MSELoss()
         
         if load_path is None:
-            self.net = Net(mult_chan=mult_chan, depth=depth)
+            self.net = nn_module.Net(mult_chan=mult_chan, depth=depth)
             if CUDA:
                 self.net.cuda()
-            self.net.apply(weights_init)
+            if init_weights:
+                print("Initializing weights")
+                self.net.apply(weights_init)
             self.optimizer = torch.optim.Adam(self.net.parameters(), lr=lr, betas=(0.5, 0.999))
             self.meta = {
                 'name': 'U-Network V0',
@@ -119,72 +123,6 @@ class Model(object):
         pred_v = self.net(signal_v)
         pred_np = pred_v.data.cpu().numpy()
         return pred_np
-
-class Net(nn.Module):
-    def __init__(self, mult_chan=16, depth=1):
-        super().__init__()
-        self.net_recurse = _Net_recurse(n_in_channels=1, mult_chan=mult_chan, depth=depth)
-        self.conv_out = torch.nn.Conv3d(mult_chan,  1, kernel_size=3, padding=1)
-        self.sig = torch.nn.Sigmoid()
-
-    def forward(self, x):
-        x_rec = self.net_recurse(x)
-        x_pre_out = self.conv_out(x_rec)
-        x_out = self.sig(x_pre_out)
-        # return x_pre_out
-        return x_out
-
-class _Net_recurse(nn.Module):
-    def __init__(self, n_in_channels, mult_chan=2, depth=0):
-        """Class for recursive definition of U-network.p
-
-        Parameters:
-        in_channels - (int) number of channels for input.
-        mult_chan - (int) factor to determine number of output channels
-        depth - (int) if 0, this subnet will only be convolutions that double the channel count.
-        """
-        super().__init__()
-        self.depth = depth
-        n_out_channels = n_in_channels*mult_chan
-        self.sub_2conv_more = SubNet2Conv(n_in_channels, n_out_channels)
-        
-        if depth > 0:
-            self.sub_2conv_less = SubNet2Conv(2*n_out_channels, n_out_channels)
-            self.pool = torch.nn.MaxPool3d(2, stride=2)
-            self.convt = torch.nn.ConvTranspose3d(2*n_out_channels, n_out_channels, kernel_size=2, stride=2)
-            self.sub_u = _Net_recurse(n_out_channels, mult_chan=2, depth=(depth - 1))
-            
-    def forward(self, x):
-        if self.depth == 0:
-            return self.sub_2conv_more(x)
-        else:  # depth > 0
-            x_2conv_more = self.sub_2conv_more(x)
-            x_pool = self.pool(x_2conv_more)
-            x_sub_u = self.sub_u(x_pool)
-            x_convt = self.convt(x_sub_u)
-            x_cat = torch.cat((x_2conv_more, x_convt), 1)  # concatenate
-            x_2conv_less = self.sub_2conv_less(x_cat)
-        return x_2conv_less
-
-class SubNet2Conv(nn.Module):
-    def __init__(self, n_in, n_out):
-        super().__init__()
-        self.conv1 = nn.Conv3d(n_in,  n_out, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm3d(n_out)
-        self.relu1 = nn.ReLU()
-        self.conv2 = nn.Conv3d(n_out, n_out, kernel_size=3, padding=1)
-        self.bn2 = nn.BatchNorm3d(n_out)
-        self.relu2 = nn.ReLU()
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu1(x)
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu2(x)
-        return x
-
 
 def weights_init(m):
     classname = m.__class__.__name__
