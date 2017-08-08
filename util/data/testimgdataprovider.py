360c6085c484ef8bbf7cca4b33f41140cea44a29
Chek Ounkomol <chek.o@outlook.com>
2017-08-08 10:30:44 -0700

    Rename WholeImgDataProvider -> TestImgDataProvider

--- util/data/testimgdataprovider.py
+++ util/data/testimgdataprovider.py
@@ -1,22 +1,50 @@
-import util.data
+import pdb
+import numpy as np
+from util.misc import pad_mirror
+from util import get_vol_transformed
 
+# For each element in a dataset, provides 1 "batch" of size 1.
 class TestImgDataProvider(object):
-    """Wrapper class to provider larger chunks for model testing."""
-    def __init__(self, dataset, dims_chunk=(32, 128, 128), dims_pin=(None, None, None)):
-        buffer_size = 1
-        batch_size = 1
+    def __init__(self, dataset,
+                 transforms=None):
+        """
+        Parameter:
+        dataset - DataSet instance
+        transforms - list/tuple of transforms to apply to dataset element
+        """
+        self._dataset = dataset
+        self._transforms = transforms
+        
+        self._count_iter = 0
         self._n_iter = len(dataset)
-        self._dims_chunk = dims_chunk
-        self._dims_pin = dims_pin
-        self._data = util.data.MultiFileDataProvider(dataset, 1, self._n_iter, batch_size, replace_interval=1,
-                                                     dims_chunk=self._dims_chunk,
-                                                     dims_pin=self._dims_pin)
 
-    def get_dims_chunk(self):
-        return self._data.get_dims_chunk()
-
+    def _make_batch(self, volumes_tuple):
+        """Change supplied 3d arrays into 5d batch array."""
+        data = []
+        for vol in volumes_tuple:
+            shape = (1, 1, ) + vol.shape
+            data.append(np.zeros(shape, dtype=vol.dtype))
+            data[-1][0, 0, ] = vol
+        return tuple(data)
+        
     def __len__(self):
-        return self._n_iter
+        return len(dataset)
 
     def __iter__(self):
-        return self._data
+        self._count_iter = 0
+        return self
+
+    def __next__(self):
+        if self._count_iter >= self._n_iter:
+            raise StopIteration
+        volumes_pre = self._dataset[self._count_iter]
+        volumes = []
+        for i, volume_pre in enumerate(volumes_pre):
+            if self._transforms is None:
+                volumes.append(volume_pre)
+            else:
+                volumes.append(get_vol_transformed(volume_pre, self._transforms[i]))
+            print('DEBUG: shape change', volume_pre.shape, volumes[-1].shape)
+        data_tuple = self._make_batch(volumes)
+        self._count_iter += 1
+        return data_tuple
