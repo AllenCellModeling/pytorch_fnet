1ecd682a69e40acbed53b49d7e8e039b6c58fd0f
Chek Ounkomol <chek.o@outlook.com>
2017-09-26 10:36:34 -0700

    Add acisimage reqs

--- docker/start_docker.sh
+++ docker/start_docker.sh
@@ -1,5 +1,8 @@
+DATA_DIR='/allen/aics/microscopy'
+
 nvidia-docker run --rm -it \
 	      -v ${PWD%/*}:/root/projects/ttf \
+	      -v ${DATA_DIR}:/root/projects/ttf/data/microscopy:ro \
 	      ${USER}/pytorch_ttf \
 	      bash
 
