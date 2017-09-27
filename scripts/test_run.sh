2f6feaba16b31870ed2de45f273fb7a355790dbe
Chek Ounkomol <chek.o@outlook.com>
2017-09-26 17:01:11 -0700

    Fix script bug

--- scripts/test_run.sh
+++ scripts/test_run.sh
@@ -10,7 +10,7 @@ PATH_DATA_TEST="data/lamin_b1_test.csv"
 cd $(cd "$(dirname ${BASH_SOURCE})" && pwd)/..
 
 python train_model.py --n_iter ${N_ITER} --buffer_size ${BUFFER_SIZE} --replace_interval -1 \
-       --path_data_train ${PATH_DATA_TEST} \
+       --path_data_train ${PATH_DATA_TRAIN} \
        --path_data_test ${PATH_DATA_TEST} \
        --batch_size 24 \
        --nn_module ttf_v8_nn \
