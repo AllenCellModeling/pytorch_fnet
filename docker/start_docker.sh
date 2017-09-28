22b64624f0836a6a8aaf7efdad808e5eae426cab
Chek Ounkomol <chek.o@outlook.com>
2017-09-28 09:29:54 -0700

    Update Docker dir names

--- docker/start_docker.sh
+++ docker/start_docker.sh
@@ -1,7 +1,7 @@
 DATA_DIR='/allen/aics/microscopy'
 
 nvidia-docker run --rm -it \
-	      -v $(cd "$(dirname ${BASH_SOURCE})"/.. && pwd):/root/projects/ttf \
+	      -v $(cd "$(dirname ${BASH_SOURCE})"/.. && pwd):/root/projects/pytorch_fnet \
 	      -v ${DATA_DIR}:${DATA_DIR}:ro \
 	      ${USER}/pytorch_ttf \
 	      bash
