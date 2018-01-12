ae3ed4074403f766ada1334d4cceb3a2f9d16c86
Gregory Johnson <gregj@alleninstitute.org>
2018-01-12 10:03:51 -0800

    added dataset to correct location

--- fnet/data/__init__.py
+++ fnet/data/__init__.py
@@ -1,4 +1,5 @@
 from fnet.data.dataset import DataSet
+from fnet.data.czidataset import CziDataset
 from fnet.data.chunkdataprovider import ChunkDataProvider
 from fnet.data.testimgdataprovider import TestImgDataProvider
 from fnet.data.functions import *
