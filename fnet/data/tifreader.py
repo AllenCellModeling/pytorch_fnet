3a3d4999b8289e8f54a7edfa2e2bc0d93d9bc000
Sharmishtaa Seshamani <sharmishtaas@OSXLTBVH8.local>
2018-01-12 16:10:33 -0800

    adding 2d tiff dataset

---
+++ fnet/data/tifreader.py
@@ -0,0 +1,17 @@
+import aicsimage.io as io
+import os
+import pdb
+
+
+
+class TifReader(object):
+    def __init__(self, filepath):
+        with io.tifReader.TifReader(filepath) as reader:
+            """Keeping it this way in order to extend it further for multi-channel tifs"""
+            self.tif_np = reader.tif.asarray()
+         
+
+    def get_image(self):
+        """Returns the image for the specified channel."""
+	"""Keeping it this way in order to extend it further for multi-channel tifs"""
+               return self.tif_np[0]
