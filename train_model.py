0e87cc8537c0cb5a35b19368cb908d7543c34c67
Chek Ounkomol <chek.o@outlook.com>
2018-01-16 15:07:58 -0800

    Simplify train_model.py and update to use new Dataset objects

--- train_model.py
+++ train_model.py
@@ -1,18 +1,17 @@
-import os
 import argparse
-import importlib
-import fnet
 import fnet.data
-import fnet.transforms
-import pandas as pd
-import numpy as np
-import torch
-import pdb
-import time
-import logging
-import sys
-import shutil
+import fnet.data.datasets
 import json
+import logging
+import model_modules.fnet_model
+import numpy as np
+import os
+import pandas as pd
+import pdb
+import shutil
+import sys
+import time
+import torch
 import warnings
 
 def main():
@@ -20,26 +19,18 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch_size', type=int, default=24, help='size of each batch')
     parser.add_argument('--buffer_size', type=int, default=5, help='number of images to cache in memory')
-    parser.add_argument('--path_train_csv', help='path to training set csv')
-    parser.add_argument('--path_test_csv', help='path to test set csv')
+    parser.add_argument('--buffer_switch_frequency', type=int, default=720, help='BufferedPatchDataset buffer switch frequency')
+    parser.add_argument('--checkpoint_testing', action='store_true', help='set to test model at checkpoints')
+    parser.add_argument('--dataset', type=str, default='dna', help='name of dataset')
     parser.add_argument('--gpu_ids', type=int, nargs='+', default=0, help='GPU ID')
     parser.add_argument('--iter_checkpoint', type=int, default=500, help='iterations between saving log/model checkpoints')
     parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
-    parser.add_argument('--model_module', default='fnet_model', help='name of the model module')
     parser.add_argument('--n_iter', type=int, default=500, help='number of training iterations')
-    parser.add_argument('--scale_z', type=float, default=0.3, help='desired um/px scale for z dimension')
-    parser.add_argument('--scale_xy', type=float, default=0.3, help='desired um/px scale for x, y dimensions')
-    parser.add_argument('--transforms_signal', nargs='+', default=['fnet.data.sub_mean_norm'], help='transform to be applied to signal images')
-    parser.add_argument('--transforms_target', nargs='+', default=['fnet.data.sub_mean_norm'], help='transform to be applied to target images')
-    parser.add_argument('--no_checkpoint_testing', action='store_true', help='set to disable testing at checkpoints')
     parser.add_argument('--nn_module', default='ttf_v8_nn', help='name of neural network module')
-    parser.add_argument('--replace_interval', type=int, default=-1, help='iterations between replacements of images in cache')
     parser.add_argument('--path_run_dir', default='saved_models', help='base directory for saved models')
+    parser.add_argument('--replace_interval', type=int, default=-1, help='iterations between replacements of images in cache')
     parser.add_argument('--seed', type=int, help='random seed')
-    parser.add_argument('--name_dataset_module', default='fnet.data.dataset', help='name of dataset module with DataSet class')
-    parser.add_argument('--choices_augmentation', nargs='+', type=(lambda x: None if x == 'None' else int(x)), help='data augmentation setting')
     opts = parser.parse_args()
-    model_module = importlib.import_module('model_modules.' + opts.model_module)
     
     if not os.path.exists(opts.path_run_dir):
         os.makedirs(opts.path_run_dir)
@@ -52,7 +43,6 @@ def main():
     fh.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
     logger.addHandler(fh)
     logger.addHandler(sh)
-#     warnings.showwarning = lambda *args, **kwargs : logger.warning(warnings.formatwarning(*args, **kwargs))
 
     #Set GPU
     main_gpu_id = opts.gpu_ids if isinstance(opts.gpu_ids, int) else opts.gpu_ids[0]
@@ -66,7 +56,7 @@ def main():
         torch.cuda.manual_seed_all(opts.seed)
 
     #Instantiate Model
-    model = model_module.Model(
+    model = model_modules.fnet_model.Model(
         nn_module=opts.nn_module,
         lr=opts.lr,
         gpu_ids=opts.gpu_ids,
@@ -79,98 +69,59 @@ def main():
         model.load_state(path_model)
         logger.info('model loaded from: {:s}'.format(path_model))
     logger.info(model)
-    
+
     #Load saved history if it already exists
     path_losses_csv = os.path.join(opts.path_run_dir, 'losses.csv')
     df_losses = pd.DataFrame()
     if os.path.exists(path_model):
         df_losses = pd.read_csv(path_losses_csv)
         logger.info('History loaded from: {:s}'.format(path_losses_csv))
-        
-    #Setup dataset
-    path_ds = os.path.join(opts.path_run_dir, 'ds.json')
-    if not os.path.exists(path_ds):
-        path_train_csv_copy = os.path.join(opts.path_run_dir, os.path.basename(opts.path_train_csv))
-        path_test_csv_copy = os.path.join(opts.path_run_dir, os.path.basename(opts.path_test_csv))
-        if not os.path.exists(path_train_csv_copy):
-            shutil.copyfile(opts.path_train_csv, path_train_csv_copy)
-        if not os.path.exists(path_test_csv_copy):
-            shutil.copyfile(opts.path_test_csv, path_test_csv_copy)
-        fnet.data.save_dataset_as_json(
-            path_train_csv = path_train_csv_copy,
-            path_test_csv = path_test_csv_copy,
-            scale_z = opts.scale_z if opts.scale_z > 0 else None,
-            scale_xy = opts.scale_xy if opts.scale_xy > 0 else None,
-            transforms_signal = opts.transforms_signal,
-            transforms_target = opts.transforms_target,
-            path_save = path_ds,
-            name_dataset_module = opts.name_dataset_module,
-        )
-    dataset = fnet.load_dataset_from_json(
-        path_load = path_ds,
+
+    ds = getattr(fnet.data.datasets, 'load_' + opts.dataset)()
+    ds_patch = fnet.data.BufferedPatchDataset(
+        dataset = ds,
+        patch_size = [32, 64, 64],
+        buffer_size = opts.buffer_size,
+        buffer_switch_frequency = opts.buffer_switch_frequency,
+        verbose = True,
     )
-    logger.info(dataset)
-
-
-    data_provider = fnet.data.ChunkDataProvider(
-        dataset,
-        buffer_size=opts.buffer_size,
-        batch_size=opts.batch_size,
-        replace_interval=opts.replace_interval,
-        choices_augmentation=opts.choices_augmentation,
+    dataloader_train = torch.utils.data.DataLoader(
+        ds_patch,
+        batch_size = opts.batch_size,
     )
 
-    data_provider_nonchunk = None
-    if not opts.no_checkpoint_testing:
-        dims_cropped = (32, '/16', '/16')
-        cropper = fnet.transforms.Cropper(dims_cropped, offsets=('mid', 0, 0))
-        transforms_nonchunk = (cropper, cropper)
-        data_provider_nonchunk = fnet.data.TestImgDataProvider(
-            dataset,
-            transforms=transforms_nonchunk,
+    dataloader_test = None
+    if opts.checkpoint_testing:
+        raise NotImplementedError
+        ds = getattr(fnet.data.datasets, 'load_' + opts.dataset)(
+            train = False,
         )
-    
+        dataloader_test = torch.utils.data.DataLoader(
+            ds,
+            batch_size = 1,
+        )
     with open(os.path.join(opts.path_run_dir, 'train_options.json'), 'w') as fo:
         json.dump(vars(opts), fo, indent=4, sort_keys=True)
 
+    iter_data = iter(dataloader_train)
     for i in range(model.count_iter, opts.n_iter):
-        x, y = data_provider.get_batch()
-        l2_batch = model.do_train_iter(x, y)
-        
-        logger.info('num_iter: {:4d} | l2_batch: {:.4f} | sources: {:s}'.format(i + 1, l2_batch, data_provider.last_sources))
+        signal, target = next(iter_data)
+        loss_batch = model.do_train_iter(signal, target)
+        logger.info('num_iter: {:4d} | loss_batch: {:.4f}'.format(i + 1, loss_batch))
         dict_iter = dict(
             num_iter = i + 1,
-            l2_batch = l2_batch,
-            sources = data_provider.last_sources,
+            loss_batch = loss_batch,
         )
         df_losses_curr = pd.concat([df_losses, pd.DataFrame([dict_iter])], ignore_index=True)
         if ((i + 1) % opts.iter_checkpoint == 0) or ((i + 1) == opts.n_iter):
-            if data_provider_nonchunk is not None:
-                # path_checkpoint_dir = os.path.join(path_run_dir, 'output_{:05d}'.format(i + 1))
-                path_checkpoint_dir = os.path.join(opts.path_run_dir, 'output')
-                data_provider_nonchunk.use_train_set()
-                dict_iter.update(fnet.test_model(
-                    model,
-                    data_provider_nonchunk,
-                    n_images = 4,
-                    path_save_dir = path_checkpoint_dir,
-                )[0])
-                data_provider_nonchunk.use_test_set()
-                dict_iter.update(fnet.test_model(
-                    model,
-                    data_provider_nonchunk,
-                    n_images = 4,
-                    path_save_dir = path_checkpoint_dir,
-                )[0])
-                df_losses_curr = pd.concat([df_losses, pd.DataFrame([dict_iter])], ignore_index=True)
+            if dataloader_test is not None:
+                raise NotImplementedError
             model.save_state(path_model)
             df_losses_curr.to_csv(path_losses_csv, index=False)
             logger.info('model saved to: {:s}'.format(path_model))
             logger.info('elapsed time: {:.1f} s'.format(time.time() - time_start))
         df_losses = df_losses_curr
-
     logger.info('total training time: {:.1f} s'.format(time.time() - time_start))
 
-    
 if __name__ == '__main__':
     main()
