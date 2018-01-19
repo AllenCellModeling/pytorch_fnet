12456ca255379f33562750669ecfea38a46ac836
Chek Ounkomol <chek.o@outlook.com>
2018-01-18 17:22:34 -0800

    Update predict.py arguments

--- predict.py
+++ predict.py
@@ -36,15 +36,15 @@ def main():
     default_resizer_str = 'fnet.transforms.Resizer((1, {:f}, {:f}))'.format(factor_yx, factor_yx)
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('--transform_signal', nargs='+', default=['fnet.transforms.normalize', default_resizer_str], help='list of transforms on Dataset signal')
-    parser.add_argument('--transform_target', nargs='+', default=['fnet.transforms.normalize', default_resizer_str], help='list of transforms on Dataset target')
     parser.add_argument('--class_dataset', default='CziDataset', help='Dataset class')
-    parser.add_argument('--path_dataset_csv', type=str, help='path to csv for constructing Dataset')
     parser.add_argument('--gpu_ids', type=int, default=0, help='GPU ID')
     parser.add_argument('--model_module', default='fnet_model', help='name of the model module')
     parser.add_argument('--n_images', type=int, default=16, help='max number of images to test')
+    parser.add_argument('--path_dataset_csv', type=str, help='path to csv for constructing Dataset')
     parser.add_argument('--path_model_dir', help='path to model directory')
     parser.add_argument('--path_save_dir', default='results', help='path to output directory')
+    parser.add_argument('--transform_signal', nargs='+', default=['fnet.transforms.normalize', default_resizer_str], help='list of transforms on Dataset signal')
+    parser.add_argument('--transform_target', nargs='+', default=['fnet.transforms.normalize', default_resizer_str], help='list of transforms on Dataset target')
     opts = parser.parse_args()
 
     model = fnet.load_model_from_dir(opts.path_model_dir, opts.gpu_ids)
