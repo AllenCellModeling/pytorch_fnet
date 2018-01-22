57282074390c614a5ec769816183d390c97ad433
Chek Ounkomol <chek.o@outlook.com>
2018-01-22 15:39:31 -0800

    Remove legacy code and files

--- fnet/data/__init__.py
+++ fnet/data/__init__.py
@@ -1,8 +1,5 @@
-from fnet.data.dataset import DataSet
 from fnet.data.czidataset import CziDataset
 from fnet.data.bufferedpatchdataset import BufferedPatchDataset
 from fnet.data.dummychunkdataset import DummyChunkDataset
 from fnet.data.tiffdataset import TiffDataset
-from fnet.data.testimgdataprovider import TestImgDataProvider
-from fnet.data.functions import *
 
