ab2fb91d531baf6d6f141fe4124a1f82644b0121
Chek Ounkomol <chek.o@outlook.com>
2018-01-25 16:04:29 -0800

    Update predict.py (remove dataloader, add options to not save transformed signal/target)

--- predict.py
+++ predict.py
@@ -10,7 +10,7 @@ import warnings
 import json
 import pdb
 
-def get_dataloader(opts):
+def get_dataset(opts):
     transform_signal = [eval(t) for t in opts.transform_signal]
     transform_target = [eval(t) for t in opts.transform_target]
     if opts.class_dataset == 'CziDataset':
@@ -29,10 +29,7 @@ def get_dataloader(opts):
         transform_target = transform_target,
     )
     print(ds)
-    return torch.utils.data.DataLoader(
-        ds,
-        batch_size = 1,
-    )
+    return ds
 
 def main():
     warnings.filterwarnings('ignore', message='.*zoom().*')
@@ -44,6 +41,8 @@ def main():
     
     parser = argparse.ArgumentParser()
     parser.add_argument('--class_dataset', default='CziDataset', help='Dataset class')
+    parser.add_argument('--dont_save_signal', action='store_true', help='set to not save signal image')
+    parser.add_argument('--dont_save_target', action='store_true', help='set to not save target image')
     parser.add_argument('--gpu_ids', type=int, default=0, help='GPU ID')
     parser.add_argument('--n_images', type=int, default=16, help='max number of images to test')
     parser.add_argument('--path_dataset_csv', type=str, help='path to csv for constructing Dataset')
@@ -55,28 +54,36 @@ def main():
 
     model = fnet.load_model_from_dir(opts.path_model_dir, opts.gpu_ids)
     print(model)
-    dataloader = get_dataloader(opts)
+    dataset = get_dataset(opts)
     entries = []
-    for i, (signal, target) in enumerate(dataloader):
+    count = 0
+    for i, data_pre in enumerate(dataset):
+        entry = {}
+        data = [torch.unsqueeze(d, 0) for d in data_pre]  # make batch of size 1
+        signal = data[0]
+        target = data[1] if (len(data) > 1) else None
         prediction = model.predict(signal)
         path_tiff_dir = os.path.join(opts.path_save_dir, '{:02d}'.format(i))
         if not os.path.exists(path_tiff_dir):
             os.makedirs(path_tiff_dir)
-        path_tiff_s = os.path.join(path_tiff_dir, 'signal.tiff')
-        path_tiff_t = os.path.join(path_tiff_dir, 'target.tiff')
+        if not opts.dont_save_signal:
+            path_tiff_s = os.path.join(path_tiff_dir, 'signal_transformed.tiff')
+            tifffile.imsave(path_tiff_s, signal.numpy()[0, ])
+            print('saved:', path_tiff_s)
+            entry['path_signal_transformed'] = os.path.relpath(path_tiff_s, opts.path_save_dir)
+        if not opts.dont_save_target and target is not None:
+            path_tiff_t = os.path.join(path_tiff_dir, 'target_transformed.tiff')
+            tifffile.imsave(path_tiff_t, target.numpy()[0, ])
+            print('saved:', path_tiff_t)
+            entry['path_target_transformed'] = os.path.relpath(path_tiff_t, opts.path_save_dir)
         path_tiff_p = os.path.join(path_tiff_dir, 'prediction.tiff')
-        tifffile.imsave(path_tiff_s, signal.numpy()[0, ])
-        print('saved:', path_tiff_s)
-        tifffile.imsave(path_tiff_t, target.numpy()[0, ])
-        print('saved:', path_tiff_t)
         tifffile.imsave(path_tiff_p, prediction.numpy()[0, ])
         print('saved:', path_tiff_p)
-        entries.append({
-            'path_signal': os.path.relpath(path_tiff_s, opts.path_save_dir),
-            'path_target': os.path.relpath(path_tiff_t, opts.path_save_dir),
-            'path_prediction': os.path.relpath(path_tiff_p, opts.path_save_dir),
-        })
-        if i >= opts.n_images:
+        entry['path_prediction'] = os.path.relpath(path_tiff_p, opts.path_save_dir)
+        
+        entries.append(entry)
+        count += 1
+        if count >= opts.n_images:
             break
     with open(os.path.join(opts.path_save_dir, 'predict_options.json'), 'w') as fo:
         json.dump(vars(opts), fo, indent=4, sort_keys=True)
