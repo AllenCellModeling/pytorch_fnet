e8f62891e6284664301078db245b259d612063da
Chek Ounkomol <chek.o@outlook.com>
2018-01-30 14:14:26 -0800

    Update prediction scripts

--- scripts/predict_2d.sh
+++ scripts/predict_2d.sh
@@ -15,6 +15,7 @@ python predict.py \
        --path_model_dir ${MODEL_DIR} \
        --path_dataset_csv "data/csvs/${DATASET}/${TEST_OR_TRAIN}.csv"\
        --n_images ${N_IMAGES} \
+       --no_prediction_unpropped \
        --path_save_dir "results/${DATASET}/${TEST_OR_TRAIN}" \
        --gpu_ids ${GPU_IDS}
 
@@ -26,5 +27,6 @@ python predict.py \
        --path_model_dir ${MODEL_DIR} \
        --path_dataset_csv "data/csvs/${DATASET}/${TEST_OR_TRAIN}.csv"\
        --n_images ${N_IMAGES} \
+       --no_prediction_unpropped \
        --path_save_dir "results/${DATASET}/${TEST_OR_TRAIN}" \
        --gpu_ids ${GPU_IDS}
