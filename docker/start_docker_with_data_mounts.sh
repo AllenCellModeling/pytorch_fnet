a97d5b47af01fe9da1fde0f534e82061ad68e50f
Chek Ounkomol <chek.o@outlook.com>
2018-02-09 14:25:43 -0800

    Add additional dataset csvs

---
+++ docker/start_docker_with_data_mounts.sh
@@ -0,0 +1,12 @@
+DATA_DIR_0='/allen/aics/modeling/data'
+DATA_DIR_1='/allen/aics/microscopy'
+DATA_DIR_2='/allen/aics/assay-dev'
+
+nvidia-docker run --rm -it \
+	      -v $(cd "$(dirname ${BASH_SOURCE})"/.. && pwd):/root/projects/pytorch_fnet \
+	      -v ${DATA_DIR_0}:${DATA_DIR_0}:ro \
+	      -v ${DATA_DIR_1}:${DATA_DIR_1}:ro \
+	      -v ${DATA_DIR_2}:${DATA_DIR_2}:ro \
+	      ${USER}/pytorch_fnet \
+	      bash
+
