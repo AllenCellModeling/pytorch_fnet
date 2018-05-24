cae8b20b102e482a249ddf4c8f6b6957233e8972
Chek Ounkomol <chek.o@outlook.com>
2018-05-24 10:58:19 -0700

    Add functionality to test on validation set while training

--- fnet/functions.py
+++ fnet/functions.py
@@ -1,15 +1,17 @@
-import fnet.fnet_model
+import importlib
 import json
 import os
 import pdb
 import sys
 
-def load_dataset_from_dir(path_dir):
-    raise NotImplementedError
+def load_model(path_model, gpu_ids=0, module='fnet_model'):
+    module_fnet_model = importlib.import_module('fnet.' + module)
+    if os.path.isdir(path_model):
+        path_model = os.path.join(path_model, 'model.p')
+    model = module_fnet_model.Model()
+    model.load_state(path_model, gpu_ids=gpu_ids)
+    return model
 
-def load_dataset_from_json(path_json):
-    raise NotImplementedError
-    
 def load_model_from_dir(path_model_dir, gpu_ids=0):
     assert os.path.isdir(path_model_dir)
     path_model_state = os.path.join(path_model_dir, 'model.p')
