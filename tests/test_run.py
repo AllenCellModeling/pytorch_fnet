16d3159a30270b723eee1a56acbfe339cf991e9d
Chek Ounkomol <chek.o@outlook.com>
2018-07-20 16:07:48 -0700

    Rename test based on test run script

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,7 +1,7 @@
 import unittest
 import subprocess
 
-class TestPredict(unittest.TestCase):
+class TestRun(unittest.TestCase):
     def test_test_run_script(self):
         """Test running test_run.sh."""
         subprocess.run(['./scripts/test_run.sh', '0'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
