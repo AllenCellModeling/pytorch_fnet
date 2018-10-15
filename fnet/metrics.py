8e031d8ea82a1ac28332349b73d4fdfe4158e520
Chek Ounkomol <chek.o@outlook.com>
2018-10-15 12:33:10 -0700

    Add module for evaluation metrics

---
+++ fnet/metrics.py
@@ -0,0 +1,35 @@
+from typing import Union
+import numpy as np
+import torch
+
+
+def corr_coef(
+        a: Union[np.ndarray, torch.Tensor],
+        b: Union[np.ndarray, torch.Tensor],
+) -> float:
+    """Calculates the Pearson correlation coefficient between the inputs.
+
+    Parameters
+    ----------
+    a
+        First input.
+    b
+        Second input.
+
+    Returns
+    -------
+    float
+        Pearson correlation coefficient between the inputs.
+
+    """
+    if isinstance(a, torch.Tensor):
+        a = a.numpy()
+    if isinstance(b, torch.Tensor):
+        b = b.numpy()
+    assert a.shape == b.shape, 'Inputs must be same shape'
+    mean_a = np.mean(a)
+    mean_b = np.mean(b)
+    std_a = np.std(a)
+    std_b = np.std(b)
+    cc = np.mean((a - mean_a)*(b - mean_b))/(std_a*std_b)
+    return cc
