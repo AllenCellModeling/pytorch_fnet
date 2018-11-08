aa62915a371f9c33092e1d2d954e462b359f8ffe
Chek Ounkomol <chek.o@outlook.com>
2018-11-08 15:10:32 -0800

    Add eval metric that looks at just first channel of two inputs

--- fnet/metrics.py
+++ fnet/metrics.py
@@ -35,3 +35,30 @@ def corr_coef(
     std_b = np.std(b)
     cc = np.mean((a - mean_a)*(b - mean_b))/(std_a*std_b)
     return cc
+
+
+def corr_coef_chan0(
+        a: Union[np.ndarray, torch.Tensor],
+        b: Union[np.ndarray, torch.Tensor],
+) -> float:
+    """Calculates the Pearson correlation coefficient between channel 0 of the
+    inputs.
+
+    Assumes the first dimension of the inputs is the channel dimension.
+
+    Parameters
+    ----------
+    a
+        First input.
+    b
+        Second input.
+
+    Returns
+    -------
+    float
+        Pearson correlation coefficient between channel 0 of the inputs.
+
+    """
+    a = a[0:1, ]
+    b = b[0:1, ]
+    return corr_coef(a, b)
