1a3299e939aa4e367eba9626a1cb4f5e1cb852b6
Chek Ounkomol <chek.o@outlook.com>
2019-03-21 16:10:10 -0700

    Add Pandas-style loc and iloc indexers to base fnet dataset

--- fnet/data/fnetdataset.py
+++ fnet/data/fnetdataset.py
@@ -14,6 +14,29 @@ def _to_str_list(olist: List) -> Optional[List[str]]:
     return [whats_my_name(o) for o in olist]
 
 
+class _LocIndexer:
+    """'Loc' indexer of objects with a 'df' (DataFrame) attribute."""
+
+    def __init__(self, super_obj):
+        assert isinstance(super_obj.df, pd.DataFrame)
+        self.super_obj = super_obj
+
+    def __getitem__(self, idx):
+        idx_trans = self.super_obj.df.index.get_loc(idx)
+        return self.super_obj[idx_trans]
+
+
+class _iLocIndexer:
+    """'iLoc' indexer of objects with a 'df' (DataFrame) attribute."""
+
+    def __init__(self, super_obj):
+        assert isinstance(super_obj.df, pd.DataFrame)
+        self.super_obj = super_obj
+
+    def __getitem__(self, idx):
+        return self.super_obj[idx]
+
+
 class FnetDataset(torch.utils.data.Dataset):
     """Abstract class for fnet datasets.
 
@@ -46,6 +69,8 @@ class FnetDataset(torch.utils.data.Dataset):
         self.transform_signal = to_objects(transform_signal)
         self.transform_target = to_objects(transform_target)
         self._metadata = None
+        self.loc = _LocIndexer(self)
+        self.iloc = _iLocIndexer(self)
 
     @property
     def metadata(self) -> dict:
