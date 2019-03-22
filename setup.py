9cbe9d5e497e002f0efe1510888a0d9077a4377e
Chek Ounkomol <chek.o@outlook.com>
2019-03-22 15:49:22 -0700

    Add cython requirement

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
     install_requires=[
+        'cython',
         'czifile==0.1.5',
         'matplotlib',
         'numpy',
