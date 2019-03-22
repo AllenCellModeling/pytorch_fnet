3fc34d942538c62c1aa93575b2f479ef9c215f95
Chek Ounkomol <chek.o@outlook.com>
2019-03-22 16:34:02 -0700

    Remove czidataset from fnet (for now?)

--- fnet/data/__init__.py
+++ fnet/data/__init__.py
@@ -1,6 +1,2 @@
-from fnet.data.czidataset import CziDataset
-from fnet.data.aicsczidataset import AICSCziDataset
 from fnet.data.bufferedpatchdataset import BufferedPatchDataset
-from fnet.data.dummychunkdataset import DummyChunkDataset
 from fnet.data.tiffdataset import TiffDataset
-
