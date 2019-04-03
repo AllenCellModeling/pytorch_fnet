4336a0833e9e5b65edf582ddb36834ce3b90124a
Chek Ounkomol <chek.o@outlook.com>
2019-04-03 14:02:42 -0700

    Update predict script to work with custom datasets

--- fnet/data/__init__.py
+++ fnet/data/__init__.py
@@ -1,5 +1,6 @@
 from fnet.data.bufferedpatchdataset import BufferedPatchDataset
 from fnet.data.tiffdataset import TiffDataset
+from fnet.data.fnetdataset import FnetDataset
 
 
-__all__ = ['BufferedPatchDataset', 'TiffDataset']
+__all__ = ['BufferedPatchDataset', 'FnetDataset', 'TiffDataset']
