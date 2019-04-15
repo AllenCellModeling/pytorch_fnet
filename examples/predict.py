f31d7b4ecb606b57290767c5ec7035fdaa09d69a
Greg Johnson <gregj@osxltthv2h.local>
2019-04-15 10:24:51 -0700

    Updates wrt MultiChDataset

--- examples/predict.py
+++ examples/predict.py
@@ -1,10 +1,4 @@
-import t4
 import os
-import tqdm
-import json
-import pandas as pd
-
-from fnet.cli.init import save_default_train_options
 
 ###################################################
 # Assume the user already ran download_and_train.py
@@ -16,13 +10,12 @@ image_save_dir = '{}/images/'.format(os.getcwd())
 model_save_dir = '{}/model/'.format(os.getcwd())
 prefs_save_path = '{}/prefs.json'.format(os.getcwd())
 
-data_save_path_test =  '{}/image_list_test.csv'.format(image_save_dir)
-
+data_save_path_test = '{}/image_list_test.csv'.format(image_save_dir)
 
 command_str = 'python  ../fnet/cli/predict.py ./ ' \
             '--dataset fnet.data.AICSTiffDataset ' \
             '--dataset_kwargs \'{{"path_csv": "{}"}}\' ' \
             '--gpu_ids {}'.format(data_save_path_test, gpu_id)
-            
+
 print(command_str)
 os.system(command_str)
