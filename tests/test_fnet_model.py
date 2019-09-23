2cd2a06d18d5a8fe310937d37973b8717ef08e72
Chek Ounkomol <chek.o@outlook.com>
2019-09-23 16:33:28 -0700

    Update tests for target weight maps

--- tests/test_fnet_model.py
+++ tests/test_fnet_model.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 import numpy as np
+import numpy.testing as npt
 import pytest
 import tifffile
 import torch
@@ -40,8 +41,7 @@ def train_new(path_model):
     )
     model.to_gpu(gpu_id)
     for idx in range(4):
-        loss = model.train_on_batch(x, y)
-        print(f'loss: {loss:7.5f}')
+        _ = model.train_on_batch(x, y)
     model.save(path_model)
 
 
@@ -50,8 +50,7 @@ def train_more(path_model):
     x, y = get_data(gpu_id)
     model = fnet.models.load_model(path_model)
     for idx in range(2):
-        loss = model.train_on_batch(x, y)
-        print(f'loss: {loss:7.5f}')
+        _ = model.train_on_batch(x, y)
     assert model.count_iter == 6
     assert model.net.some_param == SOME_PARAM_TEST_VAL
 
@@ -99,3 +98,69 @@ def test_apply_on_single_zstack(tmp_path):
     yhat_cutoff = model.apply_on_single_zstack(ar_in, cutoff=cutoff)
     assert np.issubdtype(yhat_cutoff.dtype, np.unsignedinteger)
     assert np.array_equal(yhat_cutoff, yhat_exp)
+
+
+def test_train_on_batch():
+    model = Model(nn_class='tests.data.nn_test.Net', lr=0.01)
+    shape_item = (1, 2, 4, 8)
+    batch_size = 9
+    shape_batch = (batch_size,) + shape_item
+    x_batch = torch.rand(shape_batch)
+    y_batch = x_batch * 0.666 + 0.42
+    cost_prev = float('inf')
+    for _ in range(8):
+        cost = model.train_on_batch(x_batch, y_batch)
+        assert cost < cost_prev
+        cost_prev = cost
+
+    # Test target weight maps
+    model = Model(nn_class='tests.data.nn_test.Net', lr=0.)  # disable learning
+    cost_norm = model.train_on_batch(x_batch, y_batch)
+    # Test uniform weight map
+    weight_map_batch = (
+        (torch.ones(shape_item) / np.prod(shape_item)).expand(shape_batch)
+    )
+    cost_weighted = model.train_on_batch(x_batch, y_batch, weight_map_batch)
+    npt.assert_approx_equal(cost_weighted, cost_norm)
+    # Test all-zero weight map
+    cost_weighted = model.train_on_batch(
+        x_batch, y_batch, torch.zeros(x_batch.size())
+    )
+    npt.assert_approx_equal(cost_weighted, 0.)
+    # Random weights with first and last examples having zero weight
+    weight_map_batch = torch.rand(shape_batch)
+    weight_map_batch[[0, -1]] = 0.
+    cost_weighted = model.train_on_batch(x_batch, y_batch, weight_map_batch)
+    cost_exp = (
+        model.train_on_batch(
+            x_batch[1:-1], y_batch[1:-1], weight_map_batch[1:-1]
+        )
+        * (batch_size - 2) / batch_size  # account for change in batch size
+    )
+    npt.assert_approx_equal(cost_weighted, cost_exp)
+
+
+def test_test_on_batch():
+    model = Model(nn_class='tests.data.nn_test.Net', lr=0.01)
+    shape_item = (1, 2, 4, 8)
+    batch_size = 1
+    shape_batch = (batch_size,) + shape_item
+    x_batch = torch.rand(shape_batch)
+    y_batch = x_batch * 0.666 + 0.42
+
+    # Model weights should remain the same so loss should not change
+    loss_0 = model.test_on_batch(x_batch, y_batch)
+    loss_1 = model.test_on_batch(x_batch, y_batch)
+    npt.assert_approx_equal(loss_1 - loss_0, 0.)
+
+    # Loss should remain the same with uniform weight map
+    loss_weight_uniform = model.test_on_batch(
+        x_batch, y_batch, torch.ones(shape_batch) / np.prod(shape_item)
+    )
+    npt.assert_almost_equal(loss_weight_uniform - loss_0, 0.)
+
+    # Loss should be zero with all-zero weight map
+    loss_weight_zero = model.test_on_batch(
+        x_batch, y_batch, torch.zeros(shape_batch)
+    )
+    npt.assert_almost_equal(loss_weight_zero, 0.)
