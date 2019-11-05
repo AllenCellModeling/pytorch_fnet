e2046615026ceec95ba89f904756476fb8742db8
Jackson Brown <jmaxfieldbrown@gmail.com>
2019-11-05 14:43:26 -0800

    IDE auto sort imports

--- fnet/data/multichtiffdataset.py
+++ fnet/data/multichtiffdataset.py
@@ -1,8 +1,8 @@
-from aicsimageio import AICSImage
-import pandas as pd
 import numpy as np
+import pandas as pd
 import torch
 
+from aicsimageio import AICSImage
 from fnet.data.fnetdataset import FnetDataset
 
 
