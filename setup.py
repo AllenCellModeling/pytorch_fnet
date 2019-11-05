48de0964a051a726eec320f38e92284ad50ba25c
Gregory Johnson <gregj@alleninstitute.org>
2019-11-05 09:19:57 -0800

    working download_and_train.py

--- setup.py
+++ setup.py
@@ -2,38 +2,28 @@ import setuptools
 
 
 setuptools.setup(
-    author='Ounkomol, Chek and Fernandes, Daniel A. and Seshamani, Sharmishtaa and Maleckar, Mary M. and Collman, Forrest and Johnson, Gregory R.',
-    author_email='gregj@alleninstitute.org',
-    description='A machine learning model for transforming microsocpy images between modalities',
-    entry_points={
-        'console_scripts': [
-            'fnet = fnet.cli.main:main',
-        ]
-    },
-    name='fnet',
+    author="Ounkomol, Chek and Fernandes, Daniel A. and Seshamani, Sharmishtaa and Maleckar, Mary M. and Collman, Forrest and Johnson, Gregory R.",
+    author_email="gregj@alleninstitute.org",
+    description="A machine learning model for transforming microsocpy images between modalities",
+    entry_points={"console_scripts": ["fnet = fnet.cli.main:main"]},
+    name="fnet",
     packages=setuptools.find_packages(),
-    python_requires='>=3.6',
-    url='https://github.com/AllenCellModeling/pytorch_fnet',
+    python_requires=">=3.6",
+    url="https://github.com/AllenCellModeling/pytorch_fnet",
     version="1.1",
     install_requires=[
-        'matplotlib',
-        'numpy',
-        'pandas',
-        'scipy',
-        'tifffile',
-        'torch>=1.0',
-        'tqdm',
-        'scikit-image>=0.15.0',
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "scipy",
+        "tifffile==0.15.1",
+        "torch>=1.0",
+        "tqdm",
+        "scikit-image>=0.15.0",
+        "aicsimageio==3.0.4",
     ],
     extras_require={
-        'dev': [
-            'flake8',
-            'pylint',
-            'pytest',
-            'pytest-cov',
-        ],
-        'examples': [
-            't4==0.1.3',
-        ]
+        "dev": ["flake8", "pylint", "pytest", "pytest-cov"],
+        "examples": ["t4==0.1.3"],
     },
 )
