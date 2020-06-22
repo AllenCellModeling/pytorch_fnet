be1b99e4bb8cf5e0bc6eee5b08fb54c7178d7d7f
Jianxu Chen <jianxuc@alleninstitute.org>
2020-06-22 15:32:17 -0700

    change the default patch size for inference

--- fnet/fnet_model.py
+++ fnet/fnet_model.py
@@ -359,7 +359,7 @@ class Model:
         if isinstance(x, np.ndarray):
             x = torch.from_numpy(x)
         if len(x.size()) == 4:
-            dims_max = [None, 32, 512, 512]
+            dims_max = [None, 32, 256, 256] # dims_max = [None, 32, 512, 512]
         elif len(x.size()) == 3:
             dims_max = [None, 1024, 1024]
         y_hat = _predict_piecewise_fn(
